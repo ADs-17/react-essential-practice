f908d1733b46f08e4c4ffaa1374d01915923feed
ADs-17 <anthonydsouza1728@gmail.com>
2026-02-04 17:53:48 +0530

    Composition Children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 import componentsImg from "./assets/components.png";
 
@@ -20,6 +21,15 @@ export default function App() {
             <CoreConcept />
             <CoreConcept />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
